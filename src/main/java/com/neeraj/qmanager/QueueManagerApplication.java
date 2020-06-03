5fe9a25c50bfc818fdad3fb9f7dfa2b6f68a1ae7
NeerajVishwakarma <vishwakarma.neeraj.viet@gmail.com>
2020-06-04 00:27:53 +0530

    Adding src to project

---
+++ src/main/java/com/neeraj/qmanager/QueueManagerApplication.java
@@ -0,0 +1,13 @@
+package com.neeraj.qmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neeraj/qmanager/QueueManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeraj.qmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
